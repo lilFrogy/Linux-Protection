cf812cd9ec2dc913273c423d2391be51cddd9a27
General-Bug <164042727+qwertyggd@users.noreply.github.com>
2024-03-19 15:02:52 -0400

    Update v3.sh

--- v3.sh
+++ v3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ubuntu & Kali Failsafe script <3 | my personal fav version!
-# If your having the gonverment come after you, you should probably run this and secure your system (DOES NOT SECURE EXTERNAL DRIVES{ill add such feature later <3 })
+# If your having the gov come after you(or anyone), you should probably run this and secure your system (DOES NOT SECURE EXTERNAL DRIVES{ill add such feature later <3 })
 
 # Check if running as root
 if [ "$(id -u)" != "0" ]; then

2ec14c240f12b671d794f9b976c53ef109b40b5f
General-Bug <164042727+qwertyggd@users.noreply.github.com>
2024-03-20 12:30:10 -0400

    Added comments and extra <3
    hello, world!

--- v4.sh
+++ v4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Lunux security script
+# Linux security script
 
 # -------------------
 # By - CPScript
@@ -8,11 +8,12 @@
 
 # Check if running as root
 echo "Checking for root!"
-sleep 1
+sleep 2
 clear
 
 if [ "$(id -u)" != "0" ]; then
-    echo "You must be root to run this script."
+    echo "You must be rooted to run this script."
+    echo "Please check your operating systems documentation to learn how to root your device"
     exit 1
 fi
 
@@ -24,6 +25,7 @@ VSFTPD="vsftpd"
 
 # Install firewall
 echo "Installing Firewall"
+sleep 2
 if ! dpkg -s $FIREWALL >/dev/null 2>&1; then
     apt-get -y install $FIREWALL
     ufw enable
@@ -31,21 +33,25 @@ fi
 
 # Updates
 echo "Upgrading"
+sleep 2
 apt-get -y upgrade
 apt-get -y update
 
 # Lock Out Root User
 echo "Locking Out Root User"
+sleep 2
 passwd -l root
 
 # Disable Guest Account
 echo "Disabling Guest Account"
+sleep 2
 if [ -f /etc/lightdm/lightdm.conf ]; then
     echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
 fi
 
 # Configure Password Aging Controls
 echo "Configure Password Aging Controls"
+sleep 2
 if [ -f /etc/login.defs ]; then
     debconf-set-selections <<< "login/pass_max_days 90"
     debconf-set-selections <<< "login/pass_min_days 10"
@@ -55,12 +61,14 @@ fi
 
 # Password Authentication
 echo "Password Authentication"
+sleep 2
 if [ -f /etc/pam.d/common-auth ]; then
     sed -i '1 s/^/auth optional pam_tally.so deny=5 unlock_time=900 onerr=fail audit even_deny_root_account silent\n/' /etc/pam.d/common-auth
 fi
 
 # Force Strong Passwords
-echo "FOrce Strong Passwords"
+echo "Forceing Strong Passwords"
+sleep 2
 if ! dpkg -s libpam-cracklib >/dev/null 2>&1; then
     apt-get -y install libpam-cracklib
 fi
@@ -70,12 +78,14 @@ fi
 
 # Malware removal
 echo "Malware removal!"
+sleep 2
 if [ -f /etc/apt/sources.list ]; then
     apt-get -y purge hydra* john* nikto* netcat* transmisson* wireshark*
 fi
 
 # Defult Browser & Update
 echo "Setting Default Browser and Updating!"
+sleep 2
 if ! dpkg -s firefox >/dev/null 2>&1; then
     apt-get -y install firefox
 fi
@@ -84,6 +94,7 @@ apt-get update
 
 # RootKit Scan & Removal
 echo "Atempting RootKit Scan & Removal"
+sleep 2
 if ! dpkg -s chkrootkit >/dev/null 2>&1; then
     apt-get -y install chkrootkit
 fi
@@ -92,6 +103,7 @@ apt-get purge chkrootkit -y
 
 # Fork addons
 echo "Forking Addons"
+sleep 2
 if ! dpkg -s i3 >/dev/null 2>&1; then
     apt-get -y install i3
 fi
@@ -101,12 +113,14 @@ fi
 
 # Remove unnecessary GNOME applications
 echo "Removing unnecessary GNOME applications"
+sleep 2
 if [ -f /etc/apt/sources.list ]; then
     apt-get -y remove gnome-mahjongg gnome-mines gnome-sudoku account-plugin-facebook account-plugin-flickr account-plugin-jabber account-plugin-salut account-plugin-twitter account-plugin-windows-live account-plugin-yahoo
 fi
 
 # Media removal
 echo "Media Removal"
+sleep 2
 if [ -f /etc/apt/sources.list ]; then
     echo "This will print all media to file media.txt"
     find / -type f \( -iname "*.mp3" -o -iname "*.mp4" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.jpg" -o -iname "*.wav" -o -iname "*.flac" -o -iname "*.mov" \) -print > /home/`whoami`/media.txt
@@ -114,10 +128,12 @@ fi
 
 # Package listing
 echo "Listing Packages"
+sleep 2
 dpkg -l > /home/`whoami`/packages
 
 # Install htop
 echo "Installing htop"
+sleep 2
 if ! dpkg -s htop >/dev/null 2>&1; then
     apt-get -y install htop
 fi
@@ -180,16 +196,23 @@ if [ $? -eq 0 ]; then
     sleep 3
 else
     echo "Failed to set BIOS password. Please check your system's documentation for instructions on how to set a BIOS password."
+    sleep 5
 fi
 
 # Purge unused packages
 echo "Purging unused packages"
+sleep 2
 apt-get -y purge hydra* john* nikto* netcat* transmisson* wireshark*
 
+echo 
+echo 
+echo "==========================================================="
 echo "DONE, your computer should have a decent amout of security!"
-echo "1. Should have BIOS password"
-echo "2. Shouldn't be easily brute forceable"
-echo "3. Should no longer have guest account"
-echo "4. Should have firewall"
+echo "-----------------------------------------------------------"
+echo "[+]1. Should have BIOS password"
+echo "[+]2. Shouldn't be easily brute forceable"
+echo "[+]3. Should no longer have guest account"
+echo "[+]4. Should have firewall"
+echo "------------------------------------------------------------"
 echo
 echo "If there are any issues please contact CPScript > https://github.com/CPScript"
